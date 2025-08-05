f3a9d09600d4f7f97e8a3cd8f5555b14c5321209
Robert McGregor <117464563+Zylifv@users.noreply.github.com>
2025-08-05 10:48:12 +0100

    Create Student-scores.js

---
+++ Student-scores.js
@@ -0,0 +1,44 @@
+let score = "";
+function getAverage(array) {
+  return array.reduce((a,b) => a + b) / array.length;
+}
+
+function getGrade(str) {
+  
+  if (str == 100) {
+    score = "A+";
+  } else if (str <= 99 && str >= 90) {
+    score = "A";
+  } else if (str <= 89 && str >= 80) {
+    score = "B";
+  } else if (str <= 79 && str >= 70) {
+    score = "C";
+  } else if (str <= 69 && str >= 60) {
+    score = "D";
+  } else if (str <= 59 && str >= 0) {
+    score = "F";
+  }
+  return score;
+}
+
+
+function hasPassingGrade(score) {
+  if (score <= 59) {
+    return false;
+  } else {
+    return true;
+  }
+}
+
+function studentMsg(scores, studentScore) {
+  if (studentScore <= 59) {
+    return `Class average: ${getAverage(scores)}. Your grade: ${getGrade(studentScore)}. You failed the course.`
+  } else {
+    return `Class average: ${getAverage(scores)}. Your grade: ${getGrade(studentScore)}. You passed the course.`
+  }
+}
+
+console.log(getAverage([45, 87, 98, 100, 86, 94, 67, 88, 94, 95]));
+console.log(getGrade(85));
+console.log(hasPassingGrade("B"));
+console.log(studentMsg([45, 87, 98, 100, 86, 94, 67, 88, 94, 95], 85));
