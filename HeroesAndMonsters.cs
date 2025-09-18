0f959590dae1e63ca182be3b61f0041140d87ff0
Robert McGregor <117464563+Zylifv@users.noreply.github.com>
2025-09-18 15:13:27 +0100

    Update HeroesAndMonsters.cs
     - Added earning coins after a successful battle.
     - Added a shop to spend the newly introduced coins. 
    - Added the ability to upgrade the player's weapon & stats in the shop, as well as view the upgrades via the stats screen.
     - Created a grumpy shop-keeper to sell wares.
     - Weapon upgrades and stats reset on a loss.

--- HeroesAndMonsters.cs
+++ HeroesAndMonsters.cs
@@ -1,30 +1,39 @@
 //I wanted to try and make a simple Console game about Heroes and monsters.
 //You can currently choose between 3 heroes, the Swordsman, the Archer and the Spellcaster.
 //Each one has their own unique stats for Health and Attack with scalable room for more to be added later.
-//You can see the Hero's stats at any time and also i will implement a shop with stat boosting itmes in the future.
-//The Hero must fight through 3 monsters (currently) and beat them all in a row to succeed. If they lose at any point they must go back to the first monster.
+//You can see the Hero's stats at any time and also i have not implemented a shop to upgrade weapons if you so desire.
+//The Hero must fight through 3 monsters (currently) and beat them all in a row to succeed. If they lose at any point they must go back to the first monster and all stats reset too.
 //This was a fun way to challenge myself on what i have learnt so far about the C# language.
 
-    string currentHero = "";
+    	string currentHero = "";
 		string currentHeroHealth = "";
 		string currentHeroAttack = "";
+		string currentHeroWeapon = "";
+		string startingWeapon = "";
+		int currHeroAttackStat = 0;
 		string currentMonster = "";
 		string? readResult;
 		string menuSelection = "";
 		int charMax = 3;
 		int defeatedMonsters = 0;
+		int coins = 0;
+		Random reward = new Random();
 		bool validHero = false;
 		string heroName = "";
 		string heroHealth = "";
 		string heroAttack = "";
+		string heroWeapon = "";
+		string weaponUpgrade = "";
+		bool weaponUpgradeCheck = false;
 		string monsterName = "";
 		string monsterHealth = "";
 		string monsterAttack = "";
 		string heroInfo = "";
 		bool currentBattleFinished = false;
 		
-		string[,] heroes = new string[charMax, 3];
+		string[,] heroes = new string[charMax, 4];
 		string[,] monsters = new string[charMax, 3];
+		string[] weapons = new string[charMax];
 		
 		for (int i = 0; i < charMax; i++)
 		{
@@ -34,27 +43,32 @@
 					heroName = "Swordsman";
 					heroHealth = "100";
 					heroAttack = "20";
+					heroWeapon = "Rusted Sword";
 					break;
 				case 1:
 					heroName = "Archer";
 					heroHealth = "75";
 					heroAttack = "30";
+					heroWeapon = "Wooden Bow";
 					break;
 				case 2:
 					heroName = "Spellcaster";
 					heroHealth = "50";
 					heroAttack = "35";
+					heroWeapon = "Malformed Stave";
 					break;
 				default:
 					heroName = "";
 					heroHealth = "";
 					heroAttack = "";
+					heroWeapon = "";
 					break;
 			}
 			
 			heroes[i, 0] = "Name: " + heroName;
 			heroes[i, 1] = "Health: " + heroHealth;
 			heroes[i, 2] = "Attack: " + heroAttack;
+			heroes[i, 3] = "Weapon: " + heroWeapon;
 			
 			switch(i)
 			{
@@ -84,6 +98,22 @@
 			monsters[i, 1] = "Health: " + monsterHealth;
 			monsters[i, 2] = "Attack: " + monsterAttack;
 			
+			switch(i)
+			{
+				case 0:
+					weaponUpgrade = "Maliketh's Black Blade";
+					break;
+				case 1:
+					weaponUpgrade = "Ironeye's Harp Bow";
+					break;
+				case 2:
+					weaponUpgrade = "Recluse's Crystal Staff";
+					break;
+				default:
+					break;
+			}
+			
+			weapons[i] = weaponUpgrade;
 		}
 		
 		
@@ -108,6 +138,8 @@
 		{
 			case "1":
 				//SELECT HERO
+				bool validStat = false;
+				
 				if (validHero)
 				{
 					Console.WriteLine("You have already chosen a Hero.\n");
@@ -121,7 +153,7 @@
 				{
 					if (heroes[i, 0] != "Name: ")
 					{
-						for (int j = 0; j < 3; j++)
+						for (int j = 0; j < 4; j++)
 						{
 							heroInfo += heroes[i, j].ToString() + "\n";
 						}
@@ -138,9 +170,13 @@
 							currentHero = heroes[heroCount,0].Substring(6);
 							currentHeroHealth = heroes[heroCount, 1].Substring(8);
 							currentHeroAttack = heroes[heroCount, 2].Substring(8);
+							currentHeroWeapon = heroes[heroCount, 3].Substring(8);
+							startingWeapon = heroes[heroCount, 3].Substring(8);
+							validStat = int.TryParse(currentHeroAttack, out currHeroAttackStat); //TODO				
 							Console.WriteLine($"You chose the Hero {currentHero}!\nTheir stats are:");
 							Console.WriteLine($"					{heroes[heroCount, 1]}");
-							Console.WriteLine($"					{heroes[heroCount, 2]}\n");
+							Console.WriteLine($"					{heroes[heroCount, 2]}");
+							Console.WriteLine($"					{heroes[heroCount, 3]}\n");
 							validHero = true;
 							continue;
 						} else if (readResult == "n")
@@ -165,8 +201,9 @@
 				}
 				else
 				{
-					Console.WriteLine("You current Heroes stats are:\n");
-					Console.WriteLine($"Current Hero: {currentHero}\nCurrent Hero Health: {currentHeroHealth}\nCurrent Hero Attack: {currentHeroAttack}");
+					Console.WriteLine("Your current Heroes stats are:\n");
+					Console.WriteLine($"Current Hero: {currentHero}\nCurrent Hero Health: {currentHeroHealth}\nCurrent Hero Attack: {currHeroAttackStat}\nCurrent Weapon: {currentHeroWeapon}");
+					Console.WriteLine($"\nYour current coins total: {coins}\n");
 				}
 				Console.WriteLine("Press the enter key to continue");
 				readResult = Console.ReadLine();
@@ -206,7 +243,7 @@
 							valid = int.TryParse(monsterHealth, out currentMonsterHealthStat);
 							valid = int.TryParse(monsterAttack, out currentMonsterAttackStat);
 							
-							Console.WriteLine($"Monster fight!\n\nYou must beat the {currentMonster}!");
+							Console.WriteLine($"Monster fight!\n\nYou must beat the {currentMonster}!\n");
 							//Monsters stats
 							Console.WriteLine($"The {currentMonster}'s stats are:");
 							Console.WriteLine($"					{monsters[defeatedMonsters, 1]}");
@@ -220,7 +257,7 @@
 						do
 						{
 							currentRoll = attack.Next(8, 11);
-							Console.WriteLine(currentMonsterHealthStat);
+							Console.WriteLine($"{currentMonster}'s HP: {currentMonsterHealthStat}\n");
 							currentDamage = ((currentRoll * currentHeroAttackStat) / 10);
 							currentMonsterHealthStat -= currentDamage;
 							Console.WriteLine($"{currentMonster} was damaged and lost {currentDamage} health and now has {currentMonsterHealthStat}.");
@@ -230,17 +267,24 @@
 								break;
 							}
 							currentRoll = attack.Next(8, 11);
-							Console.WriteLine(currentHeroHealthStat);
+							Console.WriteLine($"Current hero HP: {currentHeroHealthStat}\n");
 							currentDamage = ((currentRoll * currentMonsterAttackStat) / 10);
 							currentHeroHealthStat -= currentDamage;
 							Console.WriteLine($"{currentHero} was damaged and lost {currentDamage} health and now has {currentHeroHealthStat}.");
 						} while (currentHeroHealthStat > 0 && currentMonsterHealthStat > 0);
 					
-						Console.WriteLine(currentMonsterHealthStat <= 0 ? "\nYou win the round!" : "\nYou lose, game over...");
+						Console.WriteLine(currentMonsterHealthStat <= 0 ? "\nYou win the round! You have earned some coins as a reward." : "\nYou lose, game over...");
+						coins += reward.Next(5,8);
 						}
 				if (currentHeroHealthStat <= 0)
 				{
 					defeatedMonsters = 0;
+					coins = 0;
+					if (weaponUpgradeCheck == true)
+					{
+						currHeroAttackStat -= 5;
+						currentHeroWeapon = startingWeapon;
+					}
 				}
 				
 				currentMonster = "";
@@ -251,13 +295,58 @@
 				{
 					Console.WriteLine("\nYou have defeated every Monster in the game! Congratulations, you win!\n");
 					defeatedMonsters = 0;
+					weaponUpgradeCheck = false;
+					currHeroAttackStat -= 5;
+					currentHeroWeapon = startingWeapon;
 				}
 				Console.WriteLine("Press the enter key to continue");
 				readResult = Console.ReadLine();
 				break;
 			case "4":
 				//VISIT SHOP
-				Console.WriteLine("Currently under construction, please come back later.");
+				if (coins <= 0)
+				{
+					Console.WriteLine("The shop-keeper tells you that coins are required to purchase upgrades from the shop and shoos you away.\n");
+				}
+				else if (coins >= 1)
+				{
+					Console.WriteLine("The shop-keeper sees a battle worn hero enter the shop and offers you a gander at their wares...\n");
+					for (int i = 0; i < charMax; i++)
+					{
+						if (currentHero == heroes[i,0].Substring(6))
+						{
+							if (weaponUpgradeCheck == true)
+							{
+								Console.WriteLine("You already have the best available weapon for your hero. You leave the shop.");
+							}
+							if (!weaponUpgradeCheck)
+							{
+								Console.WriteLine($"The shop-keeper offers you -{weapons[i]}- for 10 coins. Do you want to purchase the upgrade? (Y/N) Current coins: {coins}");
+								readResult = Console.ReadLine();
+								if (readResult == "y")
+								{
+									if (coins >= 10)
+									{
+										Console.WriteLine($"You have purchased -{weapons[i]}- and have added 5 to your base attack.");
+										weaponUpgradeCheck = true;
+										currentHeroWeapon = weapons[i];
+										currHeroAttackStat += 5;
+										coins -= 10;
+									}
+									else
+									{
+										Console.WriteLine("The shop-keeper miffs at your lack of coin. You are unable to buy this upgrade, try fighting more monsters to earn some coin.");
+									}
+								}
+							}
+							else if (readResult == "n")
+							{
+								Console.WriteLine("You leave the shop.\n");
+							}
+						}
+					}
+					
+				}
 				Console.WriteLine("Press the enter key to continue");
 				readResult = Console.ReadLine();
 				break;
