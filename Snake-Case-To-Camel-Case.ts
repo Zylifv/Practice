652af8cc98359c616194afb31cea6ea5cb1f5bdd
Robert McGregor <117464563+Zylifv@users.noreply.github.com>
2025-09-22 18:49:28 +0100

    Create Snake-Case-To-Camel-Case.ts

---
+++ Snake-Case-To-Camel-Case.ts
@@ -0,0 +1,18 @@
+//A small challenge to take a "snake-case" string and return the same string but in Camel-case instead.
+//Example => this_is_an_example > thisIsAnExample
+
+function snakeCaseToCamelCase(value : string) {
+  let arr = [];
+  let result = "";
+  const temp = value.split("_");
+  
+  for (const el of temp) {
+    arr.push(el.charAt(0).toUpperCase() + el.slice(1));
+  }
+  for (let i = 0; i < arr.length; i++) {
+    result += arr[i];
+  }
+  return result.charAt(0).toLowerCase() + result.slice(1).toString();
+}
+
+snakeCaseToCamelCase("hello_world_with_types");
