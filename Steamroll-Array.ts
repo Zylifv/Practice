837a530dddd791a63edb27cd807296376deef218
Robert McGregor <117464563+Zylifv@users.noreply.github.com>
2025-09-25 16:15:10 +0100

    Update and rename Steamroll-Array.js to Steamroll-Array.ts

--- Steamroll-Array.ts
+++ Steamroll-Array.ts
@@ -2,16 +2,17 @@
 //The function should flatten the nested array, accounting for varying levels of nesting.
 //Your solution should not use the Array.prototype.flat() or Array.prototype.flatMap() methods and global variables should not be used.
 
-function steamrollArray(arr) {
+function steamrollArray(arr : (string | number)[]) {
   let result = [];
-
-    arr.forEach(element => {
-        if (Array.isArray(element)) {
-            result = result.concat(steamrollArray(element));
-        } else {
-            result.push(element);
-        }
-    });
-
-    return result;
+  
+  arr.forEach(el => {
+    if (Array.isArray(el)) {
+      result = result.concat(steamrollArray(el));
+    } else {
+      result.push(el);
+    }
+  });
+  return result;
 }
+
+console.log(steamrollArray([[1],[2],[["34"]],"five"]));
