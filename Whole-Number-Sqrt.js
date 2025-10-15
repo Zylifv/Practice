74e7a653a34159399cf4c7f1cf7ef861f57653e7
Robert McGregor <117464563+Zylifv@users.noreply.github.com>
2025-10-15 11:48:28 +0100

    Create Whole-Number-Sqrt.js

---
+++ Whole-Number-Sqrt.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number} x
+ * @return {number}
+ */
+
+//LeetcodeChallenge: Given a non-negative integer x, return the square root of x rounded down to the nearest integer. The returned integer should be non-negative as well.
+
+var mySqrt = function(x) {
+    return Math.floor(Math.sqrt(x))
+};
