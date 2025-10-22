e896b7b5a4661be58bc5a538287884cce284574e
Robert McGregor <117464563+Zylifv@users.noreply.github.com>
2025-10-22 16:27:23 +0100

    Create Geometry-Canvas.js

---
+++ Geometry-Canvas.js/Geometry-Canvas.js
@@ -0,0 +1,75 @@
+const sliderX = document.getElementById("slider-x");
+const sliderY = document.getElementById("slider-y");
+const sliderZ = document.getElementById("slider-z");
+let currValX = document.getElementById("value-input-x");
+let currValY = document.getElementById("value-input-y");
+let currValZ = document.getElementById("value-input-z");
+var canvas = document.createElement("canvas");
+
+function draw() {
+  
+  canvas.setAttribute("class", "canvas-board")
+  canvas.setAttribute("width", 500);
+  canvas.setAttribute("height", 500);
+  canvas.setAttribute("style", "position: absolute; x:0; y:0;");
+  document.body.appendChild(canvas);
+}
+
+draw();
+
+
+var ctx = canvas.getContext("2d");
+sliderX.oninput = function drawCube(x, y, z) {
+  
+  x = sliderX.value;
+  y = sliderY.value;
+  z = sliderZ.value
+  
+  currValX.textContent = `Current x size (px): ${x}`;
+  currValY.textContent = `Current y size (px): ${y}`;
+  currValZ.textContent = `Current z size (px): ${z}`;
+    ctx.clearRect(0, 0, 500, 500);
+    ctx.strokeRect(x,x,y,z);
+    ctx.strokeStyle = '#000'
+    ctx.beginPath();
+    ctx.moveTo(x,x)
+    ctx.stroke();
+    ctx.fill();
+  
+}
+
+sliderY.oninput = function drawCube(x, y, z) {
+
+  x = sliderX.value;
+  y = sliderY.value;
+  z = sliderZ.value
+  
+  currValX.textContent = `Current x size (px): ${x}`;
+  currValY.textContent = `Current y size (px): ${y}`;
+  currValZ.textContent = `Current z size (px): ${z}`;
+    ctx.clearRect(0, 0, 500, 500);
+    ctx.strokeRect(x,x,y,z);
+    ctx.strokeStyle = 'blue'
+    ctx.beginPath();
+    ctx.moveTo(y,y)
+    ctx.stroke();
+    ctx.fill();
+}
+
+sliderZ.oninput = function drawCube(x, y, z) {
+
+  x = sliderX.value;
+  y = sliderY.value;
+  z = sliderZ.value
+  
+  currValX.textContent = `Current x size (px): ${x}`;
+  currValY.textContent = `Current y size (px): ${y}`;
+  currValZ.textContent = `Current z size (px): ${z}`;
+    ctx.clearRect(0, 0, 500, 500);
+    ctx.strokeRect(x,x,y,z);
+    ctx.strokeStyle = 'green'
+    ctx.beginPath();
+    ctx.moveTo(y,z)
+    ctx.stroke();
+    ctx.fill();
+}
