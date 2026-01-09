9fc9a22639f0972b8a9e55f00774d8093c5852d6
Robert McGregor <117464563+Zylifv@users.noreply.github.com>
2026-01-09 14:28:08 +0000

    Update Solar-System-Planets.sql
     - added another TABLE and some more queries to see how i could expand the database

--- Solar-System-Planets.sql
+++ Solar-System-Planets.sql
@@ -10,6 +10,12 @@
       rings BIT,
       rotation VARCHAR(17)
     );
+    
+    CREATE TABLE stars (
+      name VARCHAR(20),
+      planets INT
+    );
+    
     INSERT INTO planets (name, position, surface, moons, rings, rotation) VALUES ("Mercury", 1, "Rocky", 0, 0, "Counter-clockwise");
     INSERT INTO planets (name, position, surface, moons, rings, rotation) VALUES ("Venus", 2, "Rocky", 0, 0, "Clockwise");
     INSERT INTO planets (name, position, surface, moons, rings, rotation) VALUES ("Earth", 3, "Rocky", 1, 0, "Counter-clockwise");
@@ -18,6 +24,10 @@
     INSERT INTO planets (name, position, surface, moons, rings, rotation) VALUES ("Saturn", 6, "Gaseous", 274, 1, "Counter-clockwise");
     INSERT INTO planets (name, position, surface, moons, rings, rotation) VALUES ("Uranus", 7, "Gaseous", 29, 1, "Counter-clockwise");
     INSERT INTO planets (name, position, surface, moons, rings, rotation) VALUES ("Neptune", 8, "Gaseous", 16, 1, "Counter-clockwise");
+    INSERT INTO planets (name, position, surface, moons, rings, rotation) VALUES ("Eris", 9, "Rocky", 0, 0, "Counter-clockwise");
+    DELETE FROM planets WHERE name="Eris";
+    
+    INSERT INTO stars (name, planets) VALUES ("Sun", 8), ("Proxima Centauri", 3);
 
 ---
 
@@ -35,5 +45,27 @@
 | Saturn  | 6        | Gaseous | 274   | 1     | Counter-clockwise |
 | Uranus  | 7        | Gaseous | 29    | 1     | Counter-clockwise |
 | Neptune | 8        | Gaseous | 16    | 1     | Counter-clockwise |
+
+---
+**Query #2**
+
+    
+    SELECT * FROM stars;
+
+| name             | planets |
+| ---------------- | ------- |
+| Sun              | 8       |
+| Proxima Centauri | 3       |
+
+---
+**Query #3**
+
+    
+    SELECT * FROM planets WHERE moons>90;
+
+| name    | position | surface | moons | rings | rotation          |
+| ------- | -------- | ------- | ----- | ----- | ----------------- |
+| Jupiter | 5        | Gaseous | 97    | 1     | Counter-clockwise |
+| Saturn  | 6        | Gaseous | 274   | 1     | Counter-clockwise |
 
 ---
