6cbecb29cd17ece15dea90a61632fc3185e5ed3c
Robert McGregor <117464563+Zylifv@users.noreply.github.com>
2026-01-12 13:26:50 +0000

    Create Fraction-To-Decimal.js

---
+++ Fraction-To-Decimal.js/Fraction-To-Decimal.js
@@ -0,0 +1,7 @@
+const numerator = document.getElementById("numerator");
+const denominator = document.getElementById("denominator");
+
+
+function returnDecimal(num1, num2) {
+  document.getElementById("decimal-output").textContent = num1 / num2;
+}
