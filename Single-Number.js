12f3834b80534f70ac890ecfc81d9aa4960b09fd
Robert McGregor <117464563+Zylifv@users.noreply.github.com>
2026-01-14 13:06:14 +0000

    Create Single-Number.js

---
+++ Single-Number.js
@@ -0,0 +1,26 @@
+/*Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+
+Example 1:
+
+Input: nums = [2,2,1]
+
+Output: 1
+
+Example 2:
+
+Input: nums = [4,1,2,1,2]
+
+Output: 4
+
+Example 3:
+
+Input: nums = [1]
+
+Output: 1
+*/
+
+//SOLUTION
+
+var singleNumber = function(nums) {
+    return nums.find((n) => nums.indexOf(n, nums.indexOf(n) + 1) === -1);
+};
