2312e1d5151ef3f603c5282ff9d982bd00692828
Robert McGregor <117464563+Zylifv@users.noreply.github.com>
2026-01-30 12:31:22 +0000

    Create Happy-Number.ts

---
+++ Happy-Number.ts
@@ -0,0 +1,31 @@
+//CHALLENGE
+
+/*
+
+Write an algorithm to determine if a number n is happy.
+
+A happy number is a number defined by the following process:
+
+Starting with any positive integer, replace the number by the sum of the squares of its digits.
+Repeat the process until the number equals 1 (where it will stay), or it loops endlessly in a cycle which does not include 1.
+Those numbers for which this process ends in 1 are happy.
+Return true if n is a happy number, and false if not.
+
+*/
+
+//SOLUTION
+
+let x : number = 1;
+
+var isHappy = function(n) {
+  let check = Array.from(n.toString()).map(Number);
+  let tot : number = 0;
+  check.forEach((num) => {
+    tot += Math.pow(num, 2);
+  });
+  if (x < 100) {
+    x += 1;
+    return tot == 1 ? true : isHappy(tot);
+  }
+    return false;
+};
