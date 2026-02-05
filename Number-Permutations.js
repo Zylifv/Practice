b51c3566f6d002687ae784b23caf7f78ad9a0012
Robert McGregor <117464563+Zylifv@users.noreply.github.com>
2026-02-05 16:33:40 +0000

    Update Number-Permutations.js

--- Number-Permutations.js
+++ Number-Permutations.js
@@ -20,3 +20,29 @@ var permute = function(nums) {
 };
 
 console.log(permute([1,2,3])) //returns => [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]] //PASS
+
+
+//ALTERNATE SOLUTION
+
+const arr = [1,2,3];
+
+function permute(nums) {
+  let array = [];
+  
+  if (nums.length == 0) return [];
+  if (nums.length == 1) return [nums];
+  
+  for (let i = 0; i < nums.length; i++) {
+    const currentNum = nums[i];
+    const remainNums = nums.slice(0, i).concat(nums.slice(i+1));
+    const remainNumsPermute = permute(remainNums);
+    
+    for (let j = 0; j < remainNumsPermute.length; j++) {
+      const permutedArray = [currentNum].concat(remainNumsPermute[j]);
+      array.push(permutedArray);
+    }
+  }
+  return array;
+}
+
+console.log(permute(arr)); //returns => [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]] //PASS
