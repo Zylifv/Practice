ba0ef510e6b00f7fda89320da3eaad074f822bae
Robert McGregor <117464563+Zylifv@users.noreply.github.com>
2026-02-09 12:19:30 +0000

    Update Power-Of-Four.ts

--- Power-Of-Four.ts
+++ Power-Of-Four.ts
@@ -6,8 +6,8 @@ An integer n is a power of four, if there exists an integer x such that n == 4x.
 
 //SOLUTION
 
-var isPowerOfFour = function(n) {
-    let check = n;
+var isPowerOfFour = function(n : number) {
+    let check : number = n;
     if (n == 1) {return true;}
     if (n <= 0) {return false;}
     while (check % 4 === 0) check /= 4;
@@ -16,8 +16,8 @@ var isPowerOfFour = function(n) {
 
 //ORIGINAL SOLUTION ATTEMPT (works only with positive integers)
 
-var isPowerOfFour = function(n) {
-    let check = n;
+var isPowerOfFour = function(n : number) {
+    let check : number = n;
     if (n == 1) {return true;}
     if (n < 0) {check * -1;}
     do {
